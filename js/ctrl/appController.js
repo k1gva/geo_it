c3d70f560ee046509d5a987cacd3e07cf007f97b
Saskia Geuking <k1.gva@wwu.de>
2015-12-06 11:48:56 +0000

    Kleine Fehler in der Bennenung der appController.js behoben.

--- js/ctrl/appController.js
+++ js/ctrl/appController.js
@@ -2,7 +2,7 @@ app.controller('appController', ['$scope','$http', function($scope, $http) {
 
     $scope.symbols = [];
 
-    $http.get('../code/symbols.json').success (function(data) {
+    $http.get('../symbols.json').success (function(data) {
         $scope.symbols = data;
         console.log($scope.symbols);
     });
