cc0ae3363ba3669bd4b7d00b1f5fd772c509b825
Saskia Geuking <k1.gva@wwu.de>
2015-12-07 14:38:02 +0000

    Kategorien werden jetzt in einer eigenen JSON-Datei gespeichert

--- js/ctrl/appController.js
+++ js/ctrl/appController.js
@@ -1,12 +1,16 @@
 app.controller('appController', ['$scope','$http', function($scope, $http) {
-
+    
+    $scope.categories = [];
+    
+    $http.get('../categories.json').success (function(data) {
+        $scope.categories = data;
+        console.log($scope.categories);
+    });
+    
     $scope.symbols = [];
 
     $http.get('../symbols.json').success (function(data) {
         $scope.symbols = data;
         console.log($scope.symbols);
     });
-    
-    
-    
 }]);
